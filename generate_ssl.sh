85a2fddbe08e2eb7e8660d383f31f2c80a03ce85
James <rootless4real@users.noreply.github.com>
2018-09-29 10:21:56 -0700

    Update generate_ssl.sh

--- generate_ssl.sh
+++ generate_ssl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-openssl req -newkey rsa:4096 -nodes -keyout cert/authserver/server.key -x509 -subj '/CN=localhost' -days 7300 -out cert/authserver/server.crt
-openssl req -newkey rsa:4096 -nodes -keyout cert/sessionserver/server.key -x509 -subj '/CN=localhost' -days 7300 -out cert/sessionserver/server.crtopenssl genrsa -out cert/apiserver/server.key 4096 -x509 -out cert/apiserver/server.crt
-openssl req -newkey rsa:4096 -nodes -keyout cert/apiserver/server.key -x509 -subj '/CN=localhost' -days 7300 -out cert/apiserver/server.crt
+openssl req -newkey rsa:4096 -nodes -keyout cert/authserver/certificate.key -x509 -subj '/CN=localhost' -days 7300 -out cert/authserver/certificate.crt
+openssl req -newkey rsa:4096 -nodes -keyout cert/sessionserver/certificate.key -x509 -subj '/CN=localhost' -days 7300 -out cert/sessionserver/certificate.crt
+openssl req -newkey rsa:4096 -nodes -keyout cert/apiserver/certificate.key -x509 -subj '/CN=localhost' -days 7300 -out cert/apiserver/certificate.crt
 
 echo -e " The Certificates and Keys have been generated!"
 cd ..
