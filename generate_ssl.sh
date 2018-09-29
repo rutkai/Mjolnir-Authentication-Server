3c75e3921411c75031ec45086d95d13eb4aa3bf2
James <rootless4real@users.noreply.github.com>
2018-09-29 10:12:26 -0700

    generate .crt too
    the generate_ssl.sh script was only generating the .key files, not the .crt too.  Modified it to create both. There are probably other ways of doing this too.

--- generate_ssl.sh
+++ generate_ssl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-openssl genrsa -out cert/authserver/server.key 4096
-openssl genrsa -out cert/sessionserver/server.key 4096
-openssl genrsa -out cert/apiserver/server.key 4096
+openssl req -newkey rsa:4096 -nodes -keyout cert/authserver/server.key -x509 -subj '/CN=localhost' -days 7300 -out cert/authserver/server.crt
+openssl req -newkey rsa:4096 -nodes -keyout cert/sessionserver/server.key -x509 -subj '/CN=localhost' -days 7300 -out cert/sessionserver/server.crtopenssl genrsa -out cert/apiserver/server.key 4096 -x509 -out cert/apiserver/server.crt
+openssl req -newkey rsa:4096 -nodes -keyout cert/apiserver/server.key -x509 -subj '/CN=localhost' -days 7300 -out cert/apiserver/server.crt
 
 echo -e " The Certificates and Keys have been generated!"
 cd ..
